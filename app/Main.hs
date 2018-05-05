1cb523ddbb9566ee42474165c440ffcdb4a11db7
Sergii Perynskyi <sergebass@gmail.com>
2018-05-05 00:00:44 -0400

    Reorganize the code a bit; add an option to generate the file with random numbers from this Haskell code without using external tools.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,17 @@
-module Main where
 {-# LANGUAGE OverloadedStrings #-}
-import System.IO
-import System.Environment
-import Data.List
---import Criterion
---import Criterion.Main
--- https://chrisdone.com/posts/measuring-duration-in-haskell
+
+module Main where
+
 import Control.Exception
+import Control.Monad
+import Data.List
+import Data.Monoid ((<>))
 import Formatting
 import Formatting.Clock
 import System.Clock
+import System.Environment
+import System.IO
+import System.Random
 
 atoi :: [String] -> [Int]
 atoi = map read
@@ -17,17 +19,45 @@ atoi = map read
 itoa :: [Int] -> [String]
 itoa = map show
 
-
 main :: IO ()
 main = do
-        args <- getArgs
-        start <- getTime Monotonic
-        input <- readFile(args !! 0)
-        let sorted = itoa $ sort . atoi $ lines input
-        let output  = "/tmp/hsorted-" ++ (show $ length sorted) ++ ".random"
-        hOutput <- openFile output WriteMode
-        hPutStrLn hOutput $ unlines sorted
-        hFlush hOutput
-        end <- getTime Monotonic
-        putStrLn $ "Created " ++ output
-        fprint (timeSpecs) start end
+    args <- getArgs
+    let operation : options = args
+    case operation of
+      "generate" -> generate options
+      "test-aha" -> measure "test-aha" testAha options
+      _ -> error "Incorrect operation syntax!"
+
+generate :: [String] -> IO ()
+generate args = do
+    let rowCountString : outputFile : _ = args
+    let rowCount = read rowCountString :: Int
+
+    putStrLn $ "Generating " <> outputFile <> ":"
+    hOutput <- openFile outputFile WriteMode
+
+    n <- take rowCount <$> randomNumbers
+    forM_ n $ hPutStrLn hOutput . show
+
+    hFlush hOutput
+    putStrLn $ "Generation of  " <> outputFile <> " is completed."
+
+randomNumbers :: IO [Int]
+randomNumbers = randoms <$> newStdGen
+
+measure :: String -> (FilePath -> FilePath -> IO ()) -> [String] -> IO ()
+measure title benchmark args = do
+    let inputFile : outputFile : _ = args
+    putStrLn $ "Measuring " <> title <> ":"
+    start <- getTime Monotonic
+    benchmark inputFile outputFile
+    end <- getTime Monotonic
+    fprint timeSpecs start end
+
+testAha :: FilePath -> FilePath -> IO ()
+testAha inputFile outputFile = do
+    input <- readFile inputFile
+    let sorted = itoa . sort . atoi $ lines input
+    hOutput <- openFile outputFile WriteMode
+    hPutStrLn hOutput $ unlines sorted
+    hFlush hOutput
